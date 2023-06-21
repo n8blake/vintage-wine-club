a1b3290d686dd30bbe9cbcdcfb2dca66a28ec93f
Nate <n8blake@mac.com>
2023-06-21 17:08:46 -0400

    Define IWine interface

---
+++ client/src/app/wine/wine.interface.ts
@@ -0,0 +1,34 @@
+export interface IWine {
+    _id: string;
+    name?: string;
+    description?: string;
+    year?: number;
+    type?: string;
+    color?: string;
+    location?: IWineLocation;
+    composition?: Array<IWineComponent>;
+    tastingNotes?: Array<string>;
+    imageUrl?: string;
+}
+
+export interface IWineComponent {
+    percentage?: number;
+    grape?: string;
+}
+
+export interface IWineLocation {
+    latitude?: number;
+    longitutde?: number;
+    name?: number;
+    address?: string | IAddress;
+    region?: string;
+}
+
+export interface IAddress {
+    street?: string;
+    number?: number;
+    postalCode?: number;
+    state?: string;
+    province?: string;
+    country?: string;
+}
