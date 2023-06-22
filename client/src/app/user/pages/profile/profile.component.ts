c2051099cba58085fee058fc1b5371fe153d1518
Nate <n8blake@mac.com>
2023-06-22 17:52:32 -0400

    Add services and NgForm to ProfileComponent

--- client/src/app/user/pages/profile/profile.component.ts
+++ client/src/app/user/pages/profile/profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IUser } from '../../user/user';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
+import { NgForm } from '@angular/forms';
+import { UsersManagerService } from '../../user/users-manager.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,13 +13,30 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ProfileComponent implements OnInit {
 
   user?: IUser
+  canEdit: boolean = false;
 
-  constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private authService: AuthService, private usersService: UsersManagerService) { }
 
   ngOnInit(): void {
     this.user = this.activatedRoute.snapshot.data['user']
     if(!this.user){
       this.router.navigate(['/404']);
+    }
+    if(this.authService.currentUser){
+      this.canEdit = this.authService.currentUser._id == this.user?._id;
+    }
+    
+  }
+
+  update(form: NgForm): void {
+    if(this.canEdit && this.user){
+      console.log("saving user");
+      console.log(this.user);
+      this.usersService.saveUser(this.user).subscribe(data => {
+        if(data && data._id){
+          this.user = <IUser>data;
+        }
+      });
     }
   }
 
