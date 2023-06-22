008151330cdcf66afc5760c77ceeca06a5fecda7
Nate <n8blake@mac.com>
2023-06-22 17:51:51 -0400

    Define ILocation interface

---
+++ client/src/app/interfaces/location.interface.ts
@@ -0,0 +1,14 @@
+export interface ILocation {
+    latitude?: number;
+    longitude?: number;
+    name?: string;
+    description?: string;
+    country?: string;
+    region?: string;
+    address?: string;
+    city?: string;
+    state?: string;
+    province?: string;
+    postalCode?: string;
+    zLevel?: string;
+}
