29a209a5153d6159c5383d2d91441129034a366d
Nathan Blake <n8blake@mac.com>
2023-07-03 14:55:52 -0400

    Working on Image model

--- models/Image.js
+++ models/Image.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ImageSchema = new Schema({
-    original: { type: String, required: true },
-    large: { type: String },
-    medium: { type: String },
-    small: { type: String },
-    thumbnail: { type: String }
+    filename: String,
+    originalname: String,
+    path: String
 })
 
 const Image = mongoose.model("Image", ImageSchema)
 
-module.exports = { Image };
+module.exports = Image;
