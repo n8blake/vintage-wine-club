2af45e16976a9a1c1a3d35125f6c3f379cc89eec
Nathan Blake <n8blake@mac.com>
2023-07-03 12:50:29 -0400

    Add new user route

--- client/src/app/user/user.routes.ts
+++ client/src/app/user/user.routes.ts
@@ -4,10 +4,13 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { UsersManagerComponent } from './pages/users-manager/users-manager.component';
 import { UserRouteActivatorService } from './user/user-route-activator.service';
 import { UserProfileResolverService } from './user/user-profile-resolver.service';
+import { NewUserComponent } from './pages/new-user/new-user.component';
 
 export const userRoutes: Routes = [
   { path: 'profile/:id', component: ProfileComponent,
 canActivate: [UserRouteActivatorService], resolve: {user: UserProfileResolverService} },
   { path: 'login', component: LoginComponent },
+  { path: 'manager', component: UsersManagerComponent },
+  { path: 'new', component: NewUserComponent },
   { path: '', component: UsersManagerComponent },
 ];
