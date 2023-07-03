779853d845aae817182d34511a6b91a5ce1084a2
Nathan Blake <n8blake@mac.com>
2023-07-03 14:45:02 -0400

    Add role control functions to Users Manager component

--- client/src/app/user/pages/users-manager/users-manager.component.ts
+++ client/src/app/user/pages/users-manager/users-manager.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersManagerService } from '../../user/users-manager.service';
+import { IRole, IUser } from '../../user/user';
 
 @Component({
   selector: 'app-users-manager',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersManagerComponent implements OnInit {
 
-  constructor() { }
+  users?: IUser[]
+  roles?: IRole[];
+
+  constructor(private usersService: UsersManagerService) { }
 
   ngOnInit(): void {
+    this.updateUsers();
+    this.usersService.getRoles().subscribe(roles => {
+      this.roles = roles
+    })
+  }
+
+  userHasRole(user: IUser, role: IRole): boolean {
+    if (!user.roles) return false;
+    const _role = user.roles.find(userRole => userRole._id == role._id);
+    return _role ? true : false;
+  }
+
+  addRoleToUser(user: IUser, role:IRole): void {
+    const roles = user.roles;
+    if(roles && !this.userHasRole(user, role)){
+        roles.push(role);
+    } else {
+      user.roles = [role];
+    }
+    this.usersService.saveUser(user).subscribe(updatedUser => {
+      if(!this.userHasRole(updatedUser, role)){
+        console.log('user not updated...');
+      }
+      this.updateUsers();
+    })
+  }
+
+  removeRoleFromUser(user: IUser, role:IRole): void {
+    if(user.roles && this.userHasRole(user, role)){
+      const roleIndex = user.roles.indexOf(role);
+      console.log(roleIndex);
+      user.roles.splice(roleIndex, 1);
+      this.usersService.saveUser(user).subscribe(updatedUser => {
+        if(!this.userHasRole(updatedUser, role)){
+          console.log('user not updated...');
+        }
+        this.updateUsers();
+      })
+    }
+  }
+
+  updateUsers(): void {
+    this.usersService.getUsers().subscribe(users => {
+      this.users = users
+    })
   }
 
 }
