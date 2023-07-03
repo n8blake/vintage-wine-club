041c3659267e6a3aaa892871daffe69cc9f79824
Nathan Blake <n8blake@mac.com>
2023-07-03 14:55:39 -0400

    Unprotect getUsers and roles routes for now

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,13 +5,16 @@ const withAdminRole = passport.authenticate("bearer");
 const authRoutes = require("./auth");
 const usersRoutes = require("./users");
 const protectedUserRoutes = require("./protectedUserRoutes");
+const rolesRoutes = require("./roles");
 
 // use routes
 router.use('/auth', authRoutes);
 router.use("/users", usersRoutes);
+// to protect routes, add withAdminRole 
+// e.g. router.use("/getUsers", withAdminRole, protectedUsersRoutes);
+router.use("/getUsers", protectedUserRoutes);
+router.use("/roles", rolesRoutes);
 
-// protected routes
-router.use("/getUsers", withAdminRole, protectedUserRoutes);
 
 // export
 module.exports = router;
