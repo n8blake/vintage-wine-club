387adad2bf6ecc591bba75fd7d65e210972d81a4
Nathan Blake <n8blake@mac.com>
2023-07-03 14:45:22 -0400

    Route to User profile page on login

--- client/src/app/user/pages/login/login.component.ts
+++ client/src/app/user/pages/login/login.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { ApplicationRef, Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
+import { IUser } from '../../user/user';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+
   email = '';
   password = '';
 
   mouseoverLogin = false;
   loginInvalid = false;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router, private authService: AuthService, private appRef: ApplicationRef) {}
 
   ngOnInit(): void {}
 
@@ -22,10 +24,22 @@ export class LoginComponent implements OnInit {
     this.authService
       .loginUser(form.value.email, form.value.password)
       .subscribe((response) => {
+        console.log(response);
         if (!response) {
           this.loginInvalid = true;
-        } else {
-          this.router.navigate(['/users/profile']);
+        } else if(response._id) {
+          this.authService.getCurrentIdentity().subscribe(identity => {
+            if(response._id == identity._id){
+              this.router.navigate(['/users/profile', response._id]);
+            } else {
+              console.log("error when navigating");
+              console.log(response);
+              console.log(identity);
+            }
+            //
+          });
+          //this.appRef.tick();
+          
         }
       });
   }
