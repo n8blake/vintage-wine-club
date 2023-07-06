f63315ca7c8a87e8e0d8b41dcac70addd7d43ce9
Nathan Blake <n8blake@mac.com>
2023-07-06 12:05:14 -0400

    Add wine and wine_notes routes to api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,10 +6,13 @@ const authRoutes = require("./auth");
 const usersRoutes = require("./users");
 const protectedUserRoutes = require("./protectedUserRoutes");
 const rolesRoutes = require("./roles");
-
+const winesRoutes = require("./wines");
+const notesRoutes = require("./wineNotes");
 // use routes
 router.use('/auth', authRoutes);
 router.use("/users", usersRoutes);
+router.use("/wines", winesRoutes);
+router.use("/wine_notes", notesRoutes);
 // to protect routes, add withAdminRole 
 // e.g. router.use("/getUsers", withAdminRole, protectedUsersRoutes);
 router.use("/getUsers", protectedUserRoutes);
