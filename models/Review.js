307cba1d8fef5e94814efd906691863d28b8432a
Nathan Blake <n8blake@mac.com>
2023-07-06 12:03:49 -0400

    Add more detail to Review model

--- models/Review.js
+++ models/Review.js
@@ -2,9 +2,72 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema({
-    reviewer: { type: Schema.Types.ObjectId, ref: 'User'},
-    wine: { type: Schema.Types.ObjectId, ref: 'Wine' },
-    text: { type: String }
+    reviewers: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
+    wine: { type: Schema.Types.ObjectId, ref: 'Wine', required: true },
+    reviewDate: {
+        type: Date,
+        require: true,
+        default: Date.now
+    },
+    text: { type: String },
+    visualProfile: {
+        color: { type: String },
+        clarity: { type: Number }
+    },
+    tasteProfile:{
+        tastingNotes: [
+            {
+                note: { type: Schema.Types.ObjectId, ref: 'WineNote' },
+                value: { type: Number },
+                text: { type: String }
+            }
+        ],
+        sweetness: {
+            value: { type: Number}
+        }, 
+        acidity: {
+            value: { type: Number}
+        }, 
+        tannin: {
+            value: { type: Number}
+        },
+        alcohol: {
+            value: { type: Number}
+        },
+        body: {
+            value: { type: Number}
+        },
+        finish: {
+            value: { type: Number}
+        },
+        balance: {
+            value: { type: Number}
+        },
+        effervescence: {
+            value: { type: Number}
+        },
+        bubbleSize: {
+            value: { type: Number}
+        }
+    },
+    smellProfile: {
+        smellingNotes: [
+            {
+                note: { type: Schema.Types.ObjectId, ref: 'WineNote' },
+                value: { type: Number },
+                text: { type: String }
+            }
+        ],
+        
+    },
+    images: [
+        {
+            image: { type: Schema.Types.ObjectId, ref: 'Image'}
+        }
+    ]
+    
+
+
 })
 
 const Review = mongoose.model("Review", ReviewSchema);
