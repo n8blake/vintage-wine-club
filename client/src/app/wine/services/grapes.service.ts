8d14a69868aedb17597ac4a1b5fa3e3392ec47b0
Nate <n8blake@mac.com>
2023-07-22 09:47:46 -0600

    Add getGrapes method

--- client/src/app/wine/services/grapes.service.ts
+++ client/src/app/wine/services/grapes.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IGrapeVarietal } from '../interfaces/grape-varietal';
+import { handleError } from 'src/app/errors/error-handler';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GrapesService {
+  baseURL: string = '';
+  grapesURL: string = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    if (!environment.production) {
+      this.baseURL = environment.apiURL;
+    }
+    this.grapesURL = this.baseURL + '/api/grapes/';
+  }
+
+  getGrapes(): Observable<IGrapeVarietal[]> {
+    return this.http
+      .get<IGrapeVarietal[]>(this.grapesURL)
+      .pipe(catchError(handleError<IGrapeVarietal[]>('getGrapes')));
+  }
+
 }
